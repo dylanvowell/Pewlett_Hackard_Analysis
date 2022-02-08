1a32489f6815fa34061fa3a931eb4c0c873578ec
Dylan Vowell <vowell.dylan@gmail.com>
2022-02-08 16:49:25 -0700

    Added tables from challenge

--- Queries/7.3.1 Queries.sql
+++ Queries/7.3.1 Queries.sql
@@ -79,3 +79,154 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    department_employees.to_date
+FROM retirement_info
+LEFT JOIN department_employees
+ON retirement_info.emp_no = department_employees.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN department_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+SELECT * FROM current_emp
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN department_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN department_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_count
+FROM current_emp as ce
+LEFT JOIN department_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN department_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ AND (de.to_date = '9999-01-01');
+ 
+ DROP TABLE emp_info CASCADE;
+ 
+ SELECT * FROM emp_info;
+ 
+ -- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN department_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+de.dept_no
+INTO sales_dept_retire
+FROM employees as e
+INNER JOIN department_employees AS de
+ON (de.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01') AND (de.dept_no = 'd007');
+
+SELECT * FROM retirement_info
+WHERE emp_no IN (10001);
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+de.dept_no
+INTO sales_dev_retire
+FROM employees as e
+INNER JOIN department_employees AS de
+ON (de.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01') AND (de.dept_no IN( 'd005','d007'));
+
+
+DROP TABLE extra_list_1 CASCADE;
+
+SELECT * FROM departments;
---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,31 @@
+SELECT 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	ts.title,
+	ts.from_date,
+	ts.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ts
+ON (e.emp_no = ts.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, title DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(ut.title), ut.title 
+--INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
