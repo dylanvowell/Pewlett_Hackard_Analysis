e07ec6a51642bda2355e496675be655446810cb1
Dylan Vowell <vowell.dylan@gmail.com>
2022-02-10 09:31:42 -0700

    Added some resource images, edited queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -25,7 +25,27 @@ ORDER BY emp_no, title DESC;
 SELECT * FROM unique_titles;
 
 SELECT COUNT(ut.title), ut.title 
---INTO retiring_titles
+INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+SELECT DISTINCT ON
+	(e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ts.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN department_employees as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ts
+		ON (e.emp_no = ts.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no ASC;
+
